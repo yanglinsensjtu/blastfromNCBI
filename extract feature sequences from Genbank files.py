4755315d29b0e59902dd0d0d20763419231d21bf
lsyang <yanglinsensjtu@126.com>
2019-11-08 15:50:54 +0800

    add more

---
+++ extract feature sequences from Genbank files.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov  8 15:48:28 2019
+
+@author: yanglinsen
+"""
+from Bio import SeqIO
+records = list(SeqIO.parse("../lbCpf1/addgene-plasmid-102566-\
+                           sequence-198456.gbk", "genbank"))
+first_record = records[0]
+first_record.features[9].extract(first_record)
--- getblastfromncbi.py
+++ getblastfromncbi.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 """
+Created on Fri Nov  8 12:07:04 2019
+
+@author: yanglinsen
+
 use biopytyon module to get blast results from NCBI blast
 """
 
@@ -7,8 +11,15 @@ from Bio.Blast import NCBIWWW
 from Bio import SeqIO
 records = list(SeqIO.parse('../cpf1proteins.fasta',format='fasta'))
 Cpf1protein = records[0]
-result_handle = NCBIWWW.qblast("blastp","nr", Cpf1protein.seq)
-with open("Cpf1protein_blast.xml", "w") as out_handle:
+result_handle = NCBIWWW.qblast("blastp","nr", Cpf1protein.format("fasta"))
+with open("Cpf1protein_blastp.xml", "w") as out_handle:
     out_handle.write(result_handle.read())
 result_handle.close()
+
+#using tblastn
+result_handle2 = NCBIWWW.qblast("tblastn","nt", Cpf1protein.format("fasta"))
+with open("Cpf1protein_tblastn.xml", "w") as out_handle:
+    out_handle.write(result_handle2.read())
+result_handle2.close()
+
 
