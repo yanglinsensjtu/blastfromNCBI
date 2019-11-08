fee57f7ef9264b7087013a75ac8a5342c8442d6e
lsyang <yanglinsensjtu@126.com>
2019-11-08 16:02:11 +0800

    get the sequence from genebank

--- extract feature sequences from Genbank files.py
+++ extract feature sequences from Genbank files.py
@@ -5,7 +5,7 @@ Created on Fri Nov  8 15:48:28 2019
 @author: yanglinsen
 """
 from Bio import SeqIO
-records = list(SeqIO.parse("../lbCpf1/addgene-plasmid-102566-\
-                           sequence-198456.gbk", "genbank"))
+records = list(SeqIO.parse("../lbCpf1/addgene-plasmid-102566-sequence-198456.gbk", "genbank"))
 first_record = records[0]
-first_record.features[9].extract(first_record)
+lbCpf1 = first_record.features[9].extract(first_record)
+print(lbCpf1)
