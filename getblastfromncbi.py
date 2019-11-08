6d54cf4f69cb9010c09fddcad48eebd7f0ff8593
lsyang <yanglinsensjtu@126.com>
2019-11-08 10:00:46 +0800

    fix

--- getblastfromncbi.py
+++ getblastfromncbi.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+use biopytyon module to get blast results from NCBI blast
 """
 
 from Bio.Blast import NCBIWWW
@@ -10,7 +8,6 @@ from Bio import SeqIO
 records = list(SeqIO.parse('../cpf1proteins.fasta',format='fasta'))
 Cpf1protein = records[0]
 result_handle = NCBIWWW.qblast("blastp","nr", Cpf1protein.seq)
-#result_handle = NCBIWWW.qblast("blastn", "nt", "8332116")
 with open("Cpf1protein_blast.xml", "w") as out_handle:
     out_handle.write(result_handle.read())
 result_handle.close()
